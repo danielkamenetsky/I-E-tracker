f47f4f540b77a4881f11d7819529ef1f02ce5720
Daniel Kamenetsky <20dk19@queensu.ca>
2024-08-31 12:42:01 -0400

    Added functionality to remove and add accounts

--- js/balance.js
+++ js/balance.js
@@ -1,16 +1,18 @@
+// Class to represent a bank account
 class BankAccount {
+    //Constructor to initialize the bank account with a name and balance
     constructor(name, balance) {
         this.accountName = name
         this.balance = balance
     }
 }
-
+// Class to track the balance of bank accounts
 class BankBalanceTracker {
     constructor() {
         this.accounts = []
     }
-
-    getBankBalances() { //retreive balance of each bank account
+    // Method to retrieve the balance of each bank account
+    getBankBalances() {
         while (true) {
             let accountName = prompt("Enter the name of the bank account: ")
             if (!accountName) {
@@ -27,5 +29,16 @@ class BankBalanceTracker {
             }
         }
         return this.accounts
+    }
+    // Method to add a new bank account 
+    addAccount(account) {
+        this.accounts.push(account)
+        console.log(`Account added: ${account.accountName} with balance ${account.balance}`)
+    }
+
+    // Method to remove a bank account
+    removeAccount(accountName) {
+        this.accounts = this.accounts.filter(account => account.accountName !== accountName)
+        console.log(`Account removed: ${accountName}`)
     }
 }
